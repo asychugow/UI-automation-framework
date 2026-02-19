e6e63ec59b98fa315f2daca53173fda8a337a09b
Alexander Sychugov <sychugow@gmail.com>
2026-02-19 12:03:52 +0300

    Добавил данные для ввода Логин/Пароль

--- base/base_test.py
+++ base/base_test.py
@@ -1,9 +1,12 @@
+import pytest
+from config.data import Data
 from pages.login_page import LoginPage
 from pages.dashboard_page import DashboardPage
 from pages.my_info_page import MyInfoPage
 
 class BaseTest:
 
+    data: Data
     login_page: LoginPage
     dashboard_page: DashboardPage
     my_info_page: MyInfoPage
@@ -11,6 +14,7 @@ class BaseTest:
 @pytest.fixture(autouse=True)
 def setup(self, request, driver):
     request.cls.driver = driver
+    request.cls.data = Data()
     request.cls.login_page = LoginPage(driver)
     request.cls.dashboard_page = DashboardPage(driver)
     request.cls.my_info_page = MyInfoPage(driver)
--- tests/feature_profile_test.py
+++ tests/feature_profile_test.py
@@ -5,13 +5,15 @@ class ProfileFeatureTests(BaseTest):
 
     def test_change_profile_name(self):
         self.login_page.open()
-        self.login_page.enter_login()
-        self.login_page.enter_password()
+        self.login_page.enter_login(self.data.LOGIN)
+        self.login_page.enter_password(self.data.PASSWORD)
         self.login_page.click_submit_button()
 
         self.dashboard_page.is_opened()
         self.dashboard_page.click_my_info_link()
 
         self.my_info_page.is_opened()
-        self.my_info_page.change_first_name()
+        self.my_info_page.change_first_name(f"Test {random.randint(1,50)}")
+        self.my_info_page.click_save()
+        self.my_info_page.is_changes_saved()
 
